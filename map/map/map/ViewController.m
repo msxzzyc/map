0e71e06576f5eea70a1149adedb37e19f2ab83e2
msxzzyc <10168363742qq.com>
2017-08-27 22:02:27 +0800

    corelocation框架—定位（ios8.0-）_
    步骤：导入框架，创建位置管理者，设置代理，开始定位，设置授权说明。注意默认只前台定位，后台需在target中勾选location update。注意设置位置更新的距离过滤、定位精度。

--- map/map/map/ViewController.m
+++ map/map/map/ViewController.m
@@ -7,13 +7,35 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import <CoreLocation/CoreLocation.h>
+@interface ViewController ()<CLLocationManagerDelegate>
+/** 位置管理者 */
+@property(nonatomic,strong)CLLocationManager *lM;
 @end
 
 @implementation ViewController
-
+ - (CLLocationManager *)lM
+{
+    if (!_lM) {
+        //1.创建位置管理者
+        _lM = [[CLLocationManager alloc]init];
+        //1.1设代理
+        _lM.delegate = self;
+        //每隔多远定位一次
+        _lM.distanceFilter = 100;
+        /**
+         * kCLLocationAccuracyBestForNavigation 最适合导航
+           kCLLocationAccuracyBest 最好的
+           kCLLocationAccuracyNearestTenMeters 10m
+           kCLLocationAccuracyHundredMeters 100m
+           kCLLocationAccuracyKilometer 1000m
+           kCLLocationAccuracyThreeKilometers 3000m
+         */
+        //精确度越高，越耗电，定位时间越长
+        _lM.desiredAccuracy = kCLLocationAccuracyBest;
+    }
+    return _lM;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -24,6 +46,22 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    
+    //2.使用位置管理者，开始更新用户位置
+    //默认只能前台定位
+    //后台定位需要勾选在target中勾选location update
+    [self.lM startUpdatingLocation];
+}
 
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
+{
+    NSLog(@"定位到了");
+    //停止更新
+    [manager stopUpdatingLocation];
+}
 
 @end
